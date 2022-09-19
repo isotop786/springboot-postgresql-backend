d922d5f6a29aa3815bb35b2eb6688a12330d2895
isotop786 <marufislam77@gmail.com>
2022-09-19 22:56:51 +0600

    Procfile added

--- src/main/java/com/example/crudpostgres/CrudpostgresApplication.java
+++ src/main/java/com/example/crudpostgres/CrudpostgresApplication.java
@@ -2,12 +2,21 @@ package com.example.crudpostgres;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CrudpostgresApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudpostgresApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	public String index()
+	{
+		return String.format("Application successfully working");
 	}
 
 }
